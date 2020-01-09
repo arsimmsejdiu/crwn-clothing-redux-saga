9206d16885348c9db8ffd9411f272d3ce9b2b8f9
Arsim <arsimmsejdiu@gmail.com>
2020-01-10 00:17:45 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import "./collection-item.styles.scss";
+import { connect } from "react-redux";
 
-const CollectionItem = ({ id, imageUrl, price, name }) => {
+import CustomButton from "../custom-button/custom-button.component";
+import "./collection-item.styles.scss";
+import { addItem } from "../../redux/cart/cart.actions";
+
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, price, name } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +14,15 @@ const CollectionItem = ({ id, imageUrl, price, name }) => {
         <span className="name">{name}</span>
         <span className="price">${price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+
+//Quantity property get's attached the first time around since this if block won't run when it's a new item
